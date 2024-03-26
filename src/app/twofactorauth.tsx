2eda9a1d1e180d36af2edc9c532da1fc3e40b01e
Nedim Džajić <ndzajic1@etf.unsa.ba>
2024-03-27 00:53:49 +0100

    2fa logic

--- src/app/twofactorauth.tsx
+++ src/app/twofactorauth.tsx
@@ -2,12 +2,25 @@ import React, { useState } from "react";
 import { Button, Text, View } from "react-native";
 import styles from '@/styles';
 import SixDigitInput from '../components/SixDigitInput'; // Assuming SixDigitInput is in the same directory
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const TwoFactorAuthService = require('../lib/2FAService.js');
 
 const TwoFactorAuthScreen = ({ navigation }) => {
   const [code, setCode] = useState('');
 
-  const handleVerification = () => {
-    console.log("Verification code:", code);
+  const handleVerification = async () => {
+    alert(code)
+    const user = await AsyncStorage.getItem('username'); 
+    let password = await AsyncStorage.getItem('password');
+    try{
+      let result = await TwoFactorAuthService.authenticateTwoFactorCode(code, user, password);
+      alert(result)
+
+    }
+    catch(err){
+      alert("error")
+    }
   };
 
   return (
