bfda5ba7bab1e69dbcc85055c0241e1816a121ac
Nedim Džajić <ndzajic1@etf.unsa.ba>
2024-03-27 00:31:43 +0100

    Basic frontend setup for 2fa

--- src/app/twofactorauth.tsx
+++ src/app/twofactorauth.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Text, View } from "react-native";
 import styles from '@/styles';
+import SixDigitInput from '../components/SixDigitInput'; // Assuming SixDigitInput is in the same directory
 
 const TwoFactorAuthScreen = ({ navigation }) => {
+  const [code, setCode] = useState('');
+
+  const handleVerification = () => {
+    console.log("Verification code:", code);
+  };
+
   return (
-      <View style={styles.container}>
-          <Text style={styles.title}>Two Factor Auth Screen</Text>
-          <Button title="Go to App (Privremeno)" onPress={() => navigation.navigate('App')} />
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Two Factor Auth Screen</Text>
+      <SixDigitInput value={code} onChangeText={setCode} />
+      <Button title="Verify" onPress={handleVerification} />
+      <Button title="Go to App (Privremeno)" onPress={() => navigation.navigate('App')} />
+    </View>
   );
 }
 
-export default TwoFactorAuthScreen
+export default TwoFactorAuthScreen;
