28e37b2bfeb1eb4d18a29df61a81cd757622e511
Nedim Džajić <ndzajic1@etf.unsa.ba>
2024-03-27 02:58:23 +0100

    Autofill button 2fa

--- src/app/twofactorauth.tsx
+++ src/app/twofactorauth.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Text, TouchableOpacity, View, Image } from "react-native";
 import styles from '@/styles';
 import SixDigitInput from '../components/SixDigitInput'; // Assuming SixDigitInput is in the same directory
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -10,25 +10,31 @@ const TwoFactorAuthScreen = ({ navigation }) => {
   const [code, setCode] = useState('');
 
   const handleVerification = async () => {
-    alert(code)
-    const user = await AsyncStorage.getItem('username'); 
-    let password = await AsyncStorage.getItem('password');
-    try{
-      let result = await TwoFactorAuthService.authenticateTwoFactorCode(code, user, password);
-      alert(result)
+    const user = JSON.parse(await AsyncStorage.getItem('user'));
+    console.log("User from storage:", user.username, user.password)
+
+    try {
+      let result = await TwoFactorAuthService.authenticateTwoFactorCode(code, user.username, user.password, user.token);
+      console.log(result)
 
     }
-    catch(err){
-      alert("error")
+    catch (err) {
+      console.log(err)
     }
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Two Factor Auth Screen</Text>
-      <SixDigitInput value={code} onChangeText={setCode} />
-      <Button title="Verify" onPress={handleVerification} />
-      <Button title="Go to App (Privremeno)" onPress={() => navigation.navigate('App')} />
+      <SixDigitInput value={code} onChangeText={setCode}></SixDigitInput>
+      <TouchableOpacity style={styles.loginButton} onPress={handleVerification}>
+        <Text style={styles.loginButtonText}>Authenticate</Text>
+      </TouchableOpacity>
+      <Text style={{fontSize: 12, marginBottom: 20, marginTop: 20}}>or</Text>
+      <TouchableOpacity style={styles.googleAuthButton} onPress={() => navigation.navigate('App')}>
+        <Text style={{flex:.8}}>Autofill with Google Authenticator</Text>
+        <Image source={require('../../assets/googleauth.webp')} resizeMode='contain' style={{flex:.2 }}></Image>
+      </TouchableOpacity>
     </View>
   );
 }
--- src/lib/2FAService.js
+++ src/lib/2FAService.js
@@ -12,11 +12,17 @@ const getManualEntryKey = async (username, password) => {
     }
 };
 
-const authenticateTwoFactorCode = async (code, username, password) => {
+const authenticateTwoFactorCode = async (code, username, password, token) => {
     try {
         const response = await axios.post(
-            `https://fieldlogistics-control.azurewebsites.net/api/login/authenticate/2fa?Code=${code}`,
-            { username, password }
+            `https://fieldlogistics-control.azurewebsites.net/api/login/authenticate/2fa?code=${code}`,
+            { username, password },
+            {
+                headers: {
+                  'Authorization': 'Bearer ' + token,
+                  'Content-Type': 'application/json'
+                }
+              }
         );
         return response.data; // Return the response data if successful
     } catch (error) {
