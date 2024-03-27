f99a66eabf89c635a1d042f86592c6a40b0662ec
halilovick <khalilovic02@gmail.com>
2024-03-27 12:41:45 +0100

    Used makeAuthRequest method for requests

--- src/lib/2FAService.js
+++ src/lib/2FAService.js
@@ -1,38 +1,33 @@
+import { makeAuthenticatedRequest } from "./AuthService"; // Import makeAuthenticatedRequest function
 import axios from 'axios';
 
 const getManualEntryKey = async (username, password) => {
     try {
-        const response = await axios.post(
-            "https://fieldlogistics-control.azurewebsites.net/api/login/setup/2fa",
-            { username, password }
-        );
-        return response.data.manualEntryKey; // Return the manual entry key from the response data
+        const response = await makeAuthenticatedRequest("/login/setup/2fa", null, "POST", { username, password });
+        return response.manualEntryKey;
     } catch (error) {
-        throw error; // Throw an error if the request fails
+        throw error;
     }
 };
 
 const authenticateTwoFactorCode = async (code, username, password, token) => {
     try {
-       const response = await axios.post(
+        const response = await axios.post(
             `https://fieldlogistics-control.azurewebsites.net/api/login/authenticate/2fa`,
             { username, password },
             {
                 headers: {
-                  'Authorization': 'Bearer ' + token,
-                  'Content-Type': 'application/json' 
+                    'Authorization': 'Bearer ' + token,
+                    'Content-Type': 'application/json'
                 },
-                params:{
+                params: {
                     code
                 }
-              }
-        ); 
-
-        console.log(response)
-
-        return response.status; // Return the response data if successful
+            }
+        );
+        return response.status;
     } catch (error) {
-        throw error; // Throw an error if the request fails
+        return 400;
     }
 };
 
