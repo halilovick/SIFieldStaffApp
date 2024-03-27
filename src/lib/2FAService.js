3ca8ce3b6a5e7f058e43f6fa2ff839043b8bc15a
Nedim Džajić <ndzajic1@etf.unsa.ba>
2024-03-27 05:23:47 +0100

    2fa pt 1

--- src/lib/2FAService.js
+++ src/lib/2FAService.js
@@ -14,17 +14,23 @@ const getManualEntryKey = async (username, password) => {
 
 const authenticateTwoFactorCode = async (code, username, password, token) => {
     try {
-        const response = await axios.post(
-            `https://fieldlogistics-control.azurewebsites.net/api/login/authenticate/2fa?code=${code}`,
+       const response = await axios.post(
+            `https://fieldlogistics-control.azurewebsites.net/api/login/authenticate/2fa`,
             { username, password },
             {
                 headers: {
                   'Authorization': 'Bearer ' + token,
-                  'Content-Type': 'application/json'
+                  'Content-Type': 'application/json' 
+                },
+                params:{
+                    code
                 }
               }
-        );
-        return response.data; // Return the response data if successful
+        ); 
+
+        console.log(response)
+
+        return response.status; // Return the response data if successful
     } catch (error) {
         throw error; // Throw an error if the request fails
     }
