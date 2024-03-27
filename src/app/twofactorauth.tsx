e6c80e0ff6e257c3448a08022eaa145522ee397f
Nedim Džajić <ndzajic1@etf.unsa.ba>
2024-03-27 03:32:02 +0100

    Authenticator app opens

--- src/app/twofactorauth.tsx
+++ src/app/twofactorauth.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Text, TouchableOpacity, View, Image } from "react-native";
+import { Button, Text, TouchableOpacity, View, Image, Linking } from "react-native";
 import styles from '@/styles';
 import SixDigitInput from '../components/SixDigitInput'; // Assuming SixDigitInput is in the same directory
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -16,12 +16,27 @@ const TwoFactorAuthScreen = ({ navigation }) => {
     try {
       let result = await TwoFactorAuthService.authenticateTwoFactorCode(code, user.username, user.password, user.token);
       console.log(result)
-
+      // navigation.navigate('App');
     }
     catch (err) {
       console.log(err)
     }
   };
+  const handleAutofill = async() => {
+    const user = JSON.parse(await AsyncStorage.getItem("user")) 
+    const secret = await TwoFactorAuthService.getManualEntryKey(user.username, user.password);
+    console.log("SECRET", user)
+    const appName = 'SIWeb-App'; 
+
+    const url = `otpauth://totp/${user.username}?secret=${secret}&issuer=${appName}`;
+
+    await Linking.openURL(url)
+      .then(() => console.log('Google Authenticator opened'))
+      .catch((err) => console.error('An error occurred: ', err));
+
+    handleVerification();
+     
+  }
 
   return (
     <View style={styles.container}>
@@ -31,7 +46,7 @@ const TwoFactorAuthScreen = ({ navigation }) => {
         <Text style={styles.loginButtonText}>Authenticate</Text>
       </TouchableOpacity>
       <Text style={{fontSize: 12, marginBottom: 20, marginTop: 20}}>or</Text>
-      <TouchableOpacity style={styles.googleAuthButton} onPress={() => navigation.navigate('App')}>
+      <TouchableOpacity style={styles.googleAuthButton} onPress={handleAutofill}>
         <Text style={{flex:.8}}>Autofill with Google Authenticator</Text>
         <Image source={require('../../assets/googleauth.webp')} resizeMode='contain' style={{flex:.2 }}></Image>
       </TouchableOpacity>
