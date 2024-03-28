d07f0afe552bfca2310b3695a6506e386e266c23
apehlivan1 <apehlivan1@etf.unsa.ba>
2024-03-28 06:47:37 +0100

    Login tests

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-
+import { render, fireEvent, waitFor  } from '@testing-library/react-native';
 import LoginScreen from '../app/login';
 
-describe('<LoginScreen />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<LoginScreen />).toJSON();
-    expect(tree.children.length).toBe(5);
+global.alert = jest.fn();// Mocking the alert function
+
+describe('LoginScreen', () => {
+  test('should render correctly', () => {
+    const { getByText, getByPlaceholderText } = render(<LoginScreen />);
+    
+    expect(getByPlaceholderText('Username or Phone Number')).toBeTruthy();
+    expect(getByPlaceholderText('Password')).toBeTruthy();
   });
+
+  test('should update username and password fields', () => {
+    const { getByPlaceholderText } = render(<LoginScreen />);
+
+    const usernameInput = getByPlaceholderText('Username or Phone Number');
+    const passwordInput = getByPlaceholderText('Password');
+
+    fireEvent.changeText(usernameInput, 'testUser');
+    fireEvent.changeText(passwordInput, 'testPassword');
+
+    expect(usernameInput.props.value).toBe('testUser');
+    expect(passwordInput.props.value).toBe('testPassword');
+  });
+
 });
