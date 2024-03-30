a9335fc62eafd62376b11b51762fd53cdec9113e
ndzajic1 <ndzajic1@etf.unsa.ba>
2024-03-30 20:41:41 +0100

    Simulate server calls

--- src/lib/CampaignService.js
+++ src/lib/CampaignService.js
@@ -3,55 +3,65 @@
 */
 const campaigns = [
     {
-        id: 1,
-        name: "Probna 1",
-        description: "Ovo je proba",
-        company_id: 1,
-        start_date: "2024-04-01",
-        end_date: "2024-04-07"
+        "id": 1,
+        "name": "Probna 1",
+        "description": "Ovo je proba",
+        "company_id": 1,
+        "start_date": "2024-04-01",
+        "end_date": "2024-04-07"
     },
     {
-        id: 2,
-        name: "Probna 2",
-        description: "Ovo je proba",
-        company_id: 1,
-        start_date: "2024-04-08",
-        end_date: "2024-04-14"
+        "id": 2,
+        "name": "Probna 2",
+        "description": "Ovo je proba",
+        "company_id": 1,
+        "start_date": "2024-04-08",
+        "end_date": "2024-04-14"
     },
     {
-        id: 3,
-        name: "Probna 3",
-        description: "Ovo je proba",
-        company_id: 1,
-        start_date: "2024-04-15",
-        end_date: "2024-04-21"
+        "id": 3,
+        "name": "Probna 3",
+        "description": "Ovo je proba",
+        "company_id": 1,
+        "start_date": "2024-04-15",
+        "end_date": "2024-04-21"
     }
-];
+]
 
 /*
     This function makes a server call to retrieve campaigns with the status "None" for the specified user.
     Parameters: userId (User ID of the logged-in user).
 */
 const getCampaignsForUser = async (userId) => {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => { resolve(userId) }, 1000)
+    }).then((res) => {
+        return campaigns.slice(0, 2)
+    })
 }
 
 /*
     This function makes a server call to retrieve campaigns with the status "Accepted" for the specified user.
     Parameters: userId (User ID of the logged-in user).
 */
-
 const getAcceptedCampaignsForUser = async (userId) => {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => { resolve(userId) }, 1000)
+    }).then((res) => {
+        return campaigns[2]
+    })
 }
 
 /*
     This function makes a server call to retrieve details of a specific campaign.
     Parameters: campaignId (ID of the campaign for which details are requested).
 */
-
 const getCampaignDetails = async (campaignId) => {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => { resolve(campaignId) }, 1000)
+    }).then((res) => {
+        return campaigns[campaignId - 1]
+    })
 }
 
 /*
@@ -60,15 +70,17 @@ const getCampaignDetails = async (campaignId) => {
                 campaignId (ID of the campaign to update), 
                 status (New status to set for the campaign).
 */
-
 const updateCampaignStatus = async (userId, campaignId, status) => {
-
+    return new Promise((resolve, reject) => {
+        setTimeout(() => { resolve(userId) }, 1000)
+    }).then((res) => {
+        return { "status": "OK" }
+    })
 }
 
 export {
     getCampaignsForUser,
     getAcceptedCampaignsForUser,
-    getCampaignsForUser,
     getCampaignDetails,
     updateCampaignStatus
-};
+}
