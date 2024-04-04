f6a1d474091f14204cf2472f172aad12dcaa6c19
esadinlija1 <esadinlija1@etf.unsa.ba>
2024-04-04 23:04:04 +0200

    Moved styles to separet style files

--- src/components/SixDigitInput.tsx
+++ src/components/SixDigitInput.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, TextInput, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, TextInput, TouchableOpacity, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
+import styles from '@/styles/sixdigitinputstyle';
 
 interface SixDigitInputProps {
   value: string;
@@ -55,26 +56,6 @@ const SixDigitInput: React.FC<SixDigitInputProps> = ({ value, onChangeText }) =>
 /**/
 /**/
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 40,
-    marginTop:30
-  },
-  input: {
-    width: 40,
-    height: 40,
-    borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 5,
-    textAlign: 'center',
-    margin: 5,
-  },
-  delete2faCodeButton:{
-    marginLeft:16
-  }
-});
+
 
 export default SixDigitInput;
