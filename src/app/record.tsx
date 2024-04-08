df778da722487cb5295dd9dcff50562b701f3202
esadinlija1 <esadinlija1@etf.unsa.ba>
2024-04-08 14:19:50 +0200

    Initial implementation of record screen

--- src/app/record.tsx
+++ src/app/record.tsx
@@ -6,51 +6,37 @@ import ImageInput from "@/components/ImageInput";
 
 
 const RecordDataScreen = ({ navigation }) => {
-  const [serialNumber,setSerialNumber]=useState(null);
-  const [inventoryNumber,setInventoryNumber]=useState(null);
-  const [coordinates,setCoordinates]=useState(null);
-  const [fullAdress,setFullAdress]=useState(null);
+  const [serialNumber,setSerialNumber]=useState('');
+  const [inventoryNumber,setInventoryNumber]=useState('');
+  const [coordinates,setCoordinates]=useState('');
+  const [fullAdress,setFullAdress]=useState('');
+
 
   return (
-      <View>
-         <View style={styles.inputContainer}>
-        <Text style={styles.title}>Title 1</Text>
-        <TextInput
-          style={styles.input}
-          value={serialNumber}
-          onChangeText={setSerialNumber}
-          placeholder="Input 1"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.title}>Title 2</Text>
-        <TextInput
-          style={styles.input}
-          value={inventoryNumber}
-          onChangeText={setInventoryNumber}
-          placeholder="Input 2"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.title}>Title 3</Text>
-        <TextInput
-          style={styles.input}
-          value={coordinates}
-          onChangeText={setCoordinates}
-          placeholder="Input 3"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.title}>Title 4</Text>
-        <TextInput
-          style={styles.input}
-          value={fullAdress}
-          onChangeText={setFullAdress}
-          placeholder="Input 4"
-        />
-      </View>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <View style={styles.row}>
+          <TextInput style={styles.input} placeholder="Serial number" value={serialNumber} onChangeText={setSerialNumber}/>
+          <TextInput style={styles.input} placeholder="Inventory number" value={inventoryNumber} onChangeText={setInventoryNumber}/>
+        </View>
+        <View style={styles.row}>
+        <TextInput style={styles.input} placeholder="Coordinates" value={coordinates} onChangeText={setCoordinates}/>
+        <TextInput style={styles.input} placeholder="Full address" value={fullAdress} onChangeText={setFullAdress}/>
+        </View>
+       </View>
+     
 
        <ImageInput />
+
+       <View style={styles.buttonsContainer}>
+       <TouchableOpacity style={styles.saveButton}>
+         <Text>Save</Text>
+       </TouchableOpacity>
+
+       <TouchableOpacity style={styles.cancelButton}>
+         <Text>Cancel</Text>
+       </TouchableOpacity>
+       </View>
 
       </View>
   );
--- src/styles/recordstyle.tsx
+++ src/styles/recordstyle.tsx
@@ -1,34 +1,48 @@
 import { StyleSheet } from "react-native";
 
 const styles=StyleSheet.create({
-    imageContainer:{
-        
-    },
-    inputField:{
-
-    },
+    buttonsContainer:{
+       flex:1,
+       justifyContent:'flex-end',
+       flexDirection:'row',
+       alignItems:'center'
+    }, 
     saveButton:{
-
+      paddingHorizontal:10,
+      paddingVertical:8,
+      backgroundColor:'green',
+      marginHorizontal:12
     },
     cancelButton:{
-
-    }, 
+      paddingHorizontal:10,
+      paddingVertical:8,
+      backgroundColor:'red',
+      marginHorizontal:12
+    },
     inputContainer: {
+        marginTop:40,
         marginBottom: 20,
+        flex:1
       },
-      title: {
-        fontSize: 10,
-        fontWeight: 'bold',
-        marginBottom: 5,
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop:30,
+        marginBottom:30
+      },
+      row: {
+        flexDirection: 'row',
+        marginBottom: 10,
+        width:'100%'
       },
       input: {
         borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 5,
+        borderColor: 'gray',
         padding: 10,
-        width: 250,
+        marginHorizontal: 5,
+        flex: 1,
       },
-
 })
 
 export default styles;
