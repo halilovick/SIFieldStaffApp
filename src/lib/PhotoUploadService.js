eb55de41d68c3906f2d2257664ef71de946ac285
ndzajic1 <ndzajic1@etf.unsa.ba>
2024-05-03 00:56:11 +0200

    Change request for record data

--- src/lib/PhotoUploadService.js
+++ src/lib/PhotoUploadService.js
@@ -1,11 +1,38 @@
-const FileSystem = require('expo-file-system')
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-async function uploadImage(locationId, fileUri) {
+const AuthService = require('./AuthService.js');
+const BASE_URL = "https://fieldlogistics-control.azurewebsites.net/api";
+
+const uploadImage = async (locationId, fileUri) => {
     try {
-        const fileData = await FileSystem.readAsStringAsync(fileUri, { encoding: FileSystem.EncodingType.Base64 });
-        return fileData
+        var data = new FormData();
+        data.append('DestinationLanguage', destinationLanguage);
+        data.append('Image', {
+            uri: fileUri,
+            type: 'image/jpeg',
+            name: 'image.jpeg',
+        });
+        const token = await AsyncStorage.getItem('token');
+        if (!token) {
+            return null;
+        }
+
+        const headers = {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data',
+        };
+
+        const axiosConfig = {
+            baseURL: BASE_URL,
+            url: '/location/record',
+            method: 'POST',
+            headers: headers,
+            data: data,
+        };
+        const response = await axios(axiosConfig);
+        return response.data;
     } catch (error) {
-        console.error('Error uploading file:', error);
         throw error;
     }
 }
